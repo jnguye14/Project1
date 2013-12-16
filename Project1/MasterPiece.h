9b0c34e8ff326e367d4ac626b62d41b982f1bae0
Jordan Nguyen <Jordan.N.Nguyen@asu.edu>
2013-12-16 12:51:29 -0700

    Fixed merge conflict, removed unused files

--- Project1/MasterPiece.h
+++ Project1/MasterPiece.h
@@ -16,34 +16,13 @@ public:
 	float posY;
 
 	MasterPiece(){
-<<<<<<< HEAD
-		posX=200;
-		posY=100;
-=======
 		posX = 200;
 		posY = 100;
->>>>>>> 2da49cc49dc7b69818fd2364a5c1ca7967441f54
 	}
 	~MasterPiece(){}
 
 	void toString()
 	{
-<<<<<<< HEAD
-			printf("%f \n %f",posX,posY);
-	}
-	void drawPiece()
-	{	
-
-			glColor3f(1.0,0.0,0.0);
-			glPushMatrix();
-			glBegin(GL_POLYGON);
-			glVertex2f(-0.1,-0.1);
-			glVertex2f(0.1,-0.1);
-			glVertex2f(0.1,0.1);
-			glVertex2f(-0.1,0.1);
-			glEnd();
-			glPopMatrix();		
-=======
 		printf("%f \n %f", posX, posY);
 	}
 	void drawPiece()
@@ -58,7 +37,6 @@ public:
 		glVertex2f(-0.1, 0.1);
 		glEnd();
 		glPopMatrix();
->>>>>>> 2da49cc49dc7b69818fd2364a5c1ca7967441f54
 	}
 };
 
